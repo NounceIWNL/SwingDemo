b97d3c7410bb7528d99870dac850f91009a7a641
gk <officialkepik@gmail.com>
2024-02-29 21:25:37 +0300

    initial commit

--- src/exceptions/BuiltInExceptions.java
+++ src/exceptions/BuiltInExceptions.java
@@ -5,6 +5,32 @@ import java.util.Scanner;
 
 public class BuiltInExceptions {
     public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3};
+        try {
+            int c = arr[3];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException");
+        }
+
+        TestClass testClass = null;
+        try {
+            testClass.print();
+        } catch (NullPointerException e) {
+            System.out.println("null pointer");
+        }
+
+        System.out.println("ok");
+
+        try {
+            int a = Integer.parseInt("1.2");
+        } catch (NumberFormatException e) {
+            System.err.println("number format");
+
+
+        }
+
+
         int a = 1, b;
         try {
             b = a / 0;
@@ -12,6 +38,7 @@ public class BuiltInExceptions {
             System.out.println("division by zero");
         }
         processInput();
+        proccesDividedMyZero();
     }
 
     static void processInput() {
@@ -21,9 +48,23 @@ public class BuiltInExceptions {
         try {
             a = scanner.nextInt();
         } catch (InputMismatchException e) {
-            System.out.println("Wrong format");
+            System.out.println("Wrong format");//с помощью соута
+//            e.printStackTrace();//стандартный способ вывода
         }
         System.out.println(a);
 
+    }
+
+    static void proccesDividedMyZero() {
+
+
+    }
+}
+
+class TestClass {
+    private int i = 0;
+
+    public void print() {
+        System.out.println("print");
     }
 }
